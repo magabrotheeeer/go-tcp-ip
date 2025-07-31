3cb5059a9316b820063d10991660335c28084a6c
Magomed <akhilgov_magomed@inbox.ru>
2025-07-31 17:30:37 +0300

    поменял main с учетом новых функции - промежуточный результат

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,18 @@
 package main
 
+import (
+	"github.com/magabrotheeeer/go-tcp-ip/ethernet"
+)
+
 func main() {
-	// наши сырые данные
+
 	data := []byte("Hello world, its works!")
-	_ = data
+
+	src := [6]byte{0, 0, 0, 0, 0, 0}
+	dest := [6]byte{0, 0, 0, 0, 0, 0}
+
+	ether := ethernet.New(src, dest, "test", data)
+	frame := ether.Marshal()
+	_ = frame
+
 }
